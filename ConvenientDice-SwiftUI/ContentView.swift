819f904e708b076d1f9d574197b56879329fd086
TSanni <tomas.sanni@gmail.com>
2021-11-02 18:29:21 -0500

    Finish app with random dice functionality

--- ConvenientDice-SwiftUI/ContentView.swift
+++ ConvenientDice-SwiftUI/ContentView.swift
@@ -7,10 +7,68 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View
+{
+    @State var leftDiceNum = 1
+    @State var rightDiceNum = 1
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.39, green: 0.43, blue: 0.45)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Text("Convenient")
+                        .font(Font.custom("Pacifico-Regular", size: 40))
+                    
+                    Image("diceLogo").resizable().frame(width: 100, height: 100)
+                    
+                    Text("Dice")
+                        .font(Font.custom("Pacifico-Regular", size: 40))
+
+                }
+                
+
+                Spacer()
+
+                HStack {
+                    DiceView(num: leftDiceNum)
+                    DiceView(num: rightDiceNum)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                
+                Button(action: {
+                    self.leftDiceNum = Int.random(in: 1...6)
+                    rightDiceNum = Int.random(in: 1...6)
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.black)
+                .padding(.all)
+            }
+        }
+    }
+}
+
+
+struct DiceView: View {
+    
+    let num: Int
+    
+    var body: some View {
+        Image("dice\(num)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+          //  .padding()
     }
 }
 
@@ -19,3 +77,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
