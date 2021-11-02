a531e2b09e75d63362d21acc300870aa4c202d15
TSanni <tomas.sanni@gmail.com>
2021-11-02 13:11:09 -0500

    Initial Commit

---
+++ ConvenientDice-SwiftUI/ConvenientDice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvenientDice_SwiftUIApp.swift
+//  ConvenientDice-SwiftUI
+//
+//  Created by Tomas Sanni on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ConvenientDice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
